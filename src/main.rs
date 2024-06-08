f3bbf9a353e5ac134dd3925ef6cc6e463f5f44c2
Raffaele Cataldo <kemanicataldo@gmail.com>
2024-06-07 21:12:25 -0700

    create server and routes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use std::net::SocketAddr;
+
+use axum::{extract::Query, routing::get, Json, Router};
 use chrono::Utc;
 use chrono_tz::Tz;
 use lazy_static::lazy_static;
+use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 use tzf_rs::Finder;
 
-pub type Root = Vec<Root2>;
+pub type Places = Vec<Place>;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Root2 {
+pub struct Place {
     pub place_id: i64,
     pub licence: String,
     pub osm_type: String,
@@ -22,19 +26,21 @@ pub struct Root2 {
     pub addresstype: String,
     pub name: String,
     pub display_name: String,
-    pub address: Address,
     pub boundingbox: Vec<String>,
+    pub icon: Option<String>,
+    pub namedetails: Option<Vec<String>>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Address {
-    pub village: String,
-    pub county: String,
-    pub state: String,
-    #[serde(rename = "ISO3166-2-lvl4")]
-    pub iso3166_2_lvl4: String,
-    pub country: String,
-    pub country_code: String,
+pub struct PlaceAndTime {
+    pub place: Place,
+    pub time_now: String,
+    pub time_zone: String,
+}
+
+#[derive(Deserialize)]
+struct SearchParams {
+    place: String,
 }
 
 lazy_static! {
@@ -43,25 +49,56 @@ lazy_static! {
 
 #[tokio::main]
 async fn main() {
-    let resp: Root = reqwest::Client::new()
-        .get("https://nominatim.openstreetmap.org/search?addressdetails=1&q=comptche&format=jsonv2&limit=1")
-        .header("User-Agent","Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0" )
+    let app = router().await;
+    let address = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn router() -> Router {
+    Router::new()
+        .route("/", get(check_health))
+        .route("/search", get(search))
+}
+
+async fn check_health() -> (StatusCode, String) {
+    (
+        StatusCode::OK,
+        String::from("Hello from the time zone finder!"),
+    )
+}
+
+async fn search(search: Query<SearchParams>) -> (StatusCode, Json<PlaceAndTime>) {
+    let query_string = format!(
+        "https://nominatim.openstreetmap.org/search?q={}&format=jsonv2&limit=1",
+        search.place
+    );
+    let resp: Places = reqwest::Client::new()
+        .get(query_string)
+        .header(
+            "User-Agent",
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0",
+        )
         .header("Referer", "localhost")
         .send()
         .await
-        .unwrap().json().await.unwrap();
+        .unwrap()
+        .json()
+        .await
+        .unwrap();
 
     let (lat, lon) = (
-        resp.get(0).unwrap().lat.parse().unwrap(),
-        resp.get(0).unwrap().lon.parse().unwrap(),
+        resp.first().unwrap().lat.parse().unwrap(),
+        resp.first().unwrap().lon.parse().unwrap(),
     );
-    println!("{}, {}", lat, lon);
-    // get the time zone according to the geo coordinates
     let zone = FINDER.get_tz_name(lon, lat);
-    print!("{:?}", zone);
-    // parse it into a type that implements TimeZone
     let tz: Tz = zone.parse().unwrap();
     // create a new datetime with that type
     let now = Utc::now().with_timezone(&tz);
-    println!("{}, {}, {:#?}", tz, now, resp);
+    let place_and_time = PlaceAndTime {
+        place: resp.first().unwrap().clone(),
+        time_now: now.to_string(),
+        time_zone: tz.to_string(),
+    };
+    (StatusCode::OK, Json(place_and_time))
 }
