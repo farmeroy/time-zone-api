68031a3ce9dde099d364bacdd6e8178419f0b903
Raffaele Cataldo <kemanicataldo@gmail.com>
2024-06-29 09:42:45 -0700

    use error handling

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,60 @@
+use std::num::ParseFloatError;
+
 use chrono_tz::Tz;
 use lambda_http::{run, service_fn, tracing, Error, IntoResponse, Request, RequestExt, Response};
+use lazy_static::lazy_static;
 use serde_json::json;
 use tzf_rs::Finder;
 
+lazy_static! {
+    static ref FINDER: Finder = Finder::new();
+}
+
+fn get_tz_name(lat: &str, lon: &str) -> Result<&'static str, ParseFloatError> {
+    let lat = lat.parse();
+    let lon = lon.parse();
+    match (lat, lon) {
+        (Ok(lat), Ok(lon)) => {
+            let tz = FINDER.get_tz_name(lon, lat);
+            Ok(tz)
+        }
+        (Err(e), _) | (_, Err(e)) => Err(e),
+    }
+}
+
 async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
-    let lat: f64 = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("lat"))
-        .unwrap()
-        .to_owned()
-        .parse()
-        .unwrap();
-    let lon: f64 = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("lon"))
-        .unwrap()
-        .to_owned()
-        .parse()
-        .unwrap();
-    let finder = Finder::new();
-    let zone = finder.get_tz_name(lon, lat);
-    let tz: Tz = zone.parse().unwrap();
+    let lat_query: Option<&str> = match event.query_string_parameters_ref() {
+        Some(params) => params.first("lat"),
+        None => None,
+    };
+    let lon_query: Option<&str> = match event.query_string_parameters_ref() {
+        Some(params) => params.first("lon"),
+        None => None,
+    };
+    let body;
+    if lat_query.is_some() && lon_query.is_some() {
+        body = match get_tz_name(lat_query.unwrap(), lon_query.unwrap()) {
+            Ok(res) => json!({
+                "timezone": res
+            })
+            .to_string(),
+            Err(e) => json!({
+                "error": e.to_string()
+            })
+            .to_string(),
+        }
+    } else {
+        body = json!({
+            "error": "Missing query"
+        })
+        .to_string();
+    };
+
+    //let tz: Tz = zone.parse().unwrap();
     let resp = Response::builder()
         .status(200)
         .header("content-type", "application/json")
-        .body(
-            json!({
-                "timezone": tz.name()
-            })
-            .to_string(),
-        )
+        .body(body)
         .map_err(Box::new)?;
     Ok(resp)
 }
